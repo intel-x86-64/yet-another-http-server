5732009344b73db202498f5195f11be4042d191a
Chebotarev <192.about@proton.me>
2025-10-21 19:16:44 +0300

    Add requestProcessing, listenSocket,

--- src/Server/Server.cpp
+++ src/Server/Server.cpp
@@ -1,8 +1,11 @@
 #include "Server.h"
 #include <iostream>
 #include <netinet/in.h>
+#include <sstream>
 #include <stdexcept>
+#include <string>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 void Server::createSocket() {
@@ -37,7 +40,47 @@ Server::Server() : jsonParser("config.json"), pageParser(jsonParser) {
   }
 }
 
-void Server::listenSocket() {}
+std::string Server::requestProcessing(std::string request) {
+  std::istringstream stream(request);
+
+  std::string method, path, protocol;
+  stream >> method >> path >> protocol;
+
+  return pageParser.parsePage(path);
+}
+
+void Server::handleClient(int clientSocket) {
+  char buffer[1024]{0};
+  ssize_t bytesRead = read(clientSocket, buffer, 1023);
+  if (bytesRead < 0) {
+    throw std::runtime_error("Error read buffer");
+  }
+
+  std::string request{buffer};
+  std::string responce = requestProcessing(request);
+
+  send(clientSocket, responce.c_str(), responce.size(), 0);
+}
+
+void Server::listenSocket() {
+  if (listen(serverSocket, 3) == -1) {
+    throw std::runtime_error("Error to listen socket");
+  }
+
+  while (true) {
+    int clientSocket = accept(serverSocket, 0, 0);
+
+    if (clientSocket < 0) {
+      throw std::runtime_error("Error to accept client");
+    }
+
+    try {
+      handleClient(clientSocket);
+    } catch (const std::runtime_error &ex) {
+      std::cerr << ex.what() << std::endl;
+    }
+  }
+}
 
 void Server::start() {
   this->setupAddress();
@@ -48,6 +91,8 @@ void Server::start() {
   } catch (const std::runtime_error &ex) {
     std::cerr << ex.what() << std::endl;
   }
+
+  this->listenSocket();
 }
 
 Server::~Server() {
