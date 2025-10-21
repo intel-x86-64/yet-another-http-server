f8a60a202fb003393e180aae9a688082921a3b3e
Chebotarev <192.about@proton.me>
2025-10-21 23:12:51 +0300

    update main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "JsonParser/JsonParser.h"
+#include "PageParser/PageParser.h"
+#include "Server/Server.h"
+#include <iostream>
+#include <ostream>
+#include <stdexcept>
+
+int main() {
+  Server server;
+  server.start();
+}
